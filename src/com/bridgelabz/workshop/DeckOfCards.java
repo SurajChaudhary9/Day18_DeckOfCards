21be9a81d3053c37f5ac39f00d8fdfdd1c7d79f4
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-03-07 08:28:51 +0530

    Added : Welcome Message On Master Branch

---
+++ src/com/bridgelabz/workshop/DeckOfCards.java
@@ -0,0 +1,17 @@
+/**
+ *@PROBLEM_STATEMENT Welcome Message
+ *@author Suraj Chaudhary
+ *@Date 07-Mar-2022
+ */
+package com.bridgelabz.workshop;
+
+public class DeckOfCards {
+	public void welcome() {
+		System.out.println("Welcome to the Deck of Card DashBoard");
+	}
+
+	public static void main(String[] args) {
+		DeckOfCards deckOfCardsGame = new DeckOfCards();
+		deckOfCardsGame.welcome();
+	}
+}
